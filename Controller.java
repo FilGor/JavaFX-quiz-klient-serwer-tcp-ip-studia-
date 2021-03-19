9fd51236c6b0d415d83010414559340a89e61b49
FilGor <FilGor@github.com>
2021-03-19 19:29:06 +0100

    backup

--- Controller.java
+++ Controller.java
@@ -1,32 +1,24 @@
 package sample;
 
     import javafx.fxml.FXML;
-    import javafx.scene.text.Text;
     import javafx.scene.text.TextFlow;
 
     import java.io.BufferedReader;
-    import java.io.FileReader;
     import java.io.IOException;
-    import java.nio.file.FileSystems;
+    import java.io.InputStreamReader;
+    import java.net.ServerSocket;
+    import java.net.Socket;
     import java.nio.file.Files;
-    import java.nio.file.Path;
     import java.nio.file.Paths;
     import java.util.*;
-
-    import java.util.stream.Collector;
     import java.util.stream.Collectors;
-    import java.util.stream.IntStream;
-    import java.util.stream.Stream;
+
 
 
 public class Controller {
 
         @FXML
         private TextFlow SpaceForText;
-
-    public Controller() throws IOException {
-    }
-
 
     public void initialize() throws IOException {
 
@@ -36,7 +28,7 @@ public class Controller {
 
 
         Map<String, String> mapFromFile = Files.lines(Paths.get(filepath))
-                .filter(s -> s.matches(".*|.*"))
+                .filter(s -> s.matches(".*|.*")) //bierzemy pod uwage linie w ktorych mamy jakies znaki rozdzielone |
                 .collect(Collectors.toMap(k -> k.split("\\|")[0], v -> v.split("\\|")[1]));
 
         for (String key : mapFromFile.keySet())
@@ -45,7 +37,5 @@ public class Controller {
         }
 
 
-
     }
-
     }
--- Main.java
+++ Main.java
@@ -6,6 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 public class Main extends Application {
 
     @Override
@@ -13,12 +20,37 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 500));
+
         primaryStage.show();
+
+        new Thread(()->{
+            try {
+                dd();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    public void dd() throws IOException {
+        ServerSocket ss = new ServerSocket(8888); //utworzenie gniazda i portu
+for(;;) {
+
+    Socket s = ss.accept(); // oczekiwanie na polaczenie
+
+    InputStreamReader in = new InputStreamReader(s.getInputStream());
+    BufferedReader bf = new BufferedReader(in);
+
+    String str = bf.readLine();
+
+
+    System.out.println(str);
+}}
 
 }
